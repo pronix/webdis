c26d31e6b6deeadcd14f02e3dbfcb5fc51d3c138
dima <pronix.service@gmail.com>
2014-08-03 20:00:43 +0200

    improve security

--- recipes/default.rb
+++ recipes/default.rb
@@ -10,27 +10,33 @@
 package 'redis' do
 	action :install
 end
-
-package "libevent-devel" do
+package 'nginx' do
+	action :install
+end
+package 'httpd-tools' do
 	action :install
 end
 
-git "/tmp/webdis" do
-	repository "git://github.com/nicolasff/webdis.git"
-	reference "master"
+package 'libevent-devel' do
+	action :install
+end
+
+git '/tmp/webdis' do
+	repository 'git://github.com/nicolasff/webdis.git'
+	reference 'master'
 	action :checkout
 end
 
-bash "install-webdis" do
+bash 'install-webdis' do
 	code <<-EOC
 		cd /tmp/webdis
 		make
     make install
 	EOC
-  not_if { File.exists? "/usr/local/bin/webdis" }
+  not_if { File.exists? '/usr/local/bin/webdis' }
 end
 
-service "iptables" do
+service 'iptables' do
 	action [:disable, :stop]
 end
 
@@ -38,4 +44,61 @@ service 'redis' do
 	action [:enable, :start]
 end
 
-execute "sudo -u nobody /usr/local/bin/webdis &"
+execute 'sudo -u nobody /usr/local/bin/webdis &'
+
+package 'libevent-devel' do
+	action :install
+end
+
+git '/tmp/webdis' do
+	repository 'git://github.com/nicolasff/webdis.git'
+	reference 'master'
+	action :checkout
+end
+
+bash 'install-webdis' do
+	code <<-EOC
+		cd /tmp/webdis
+		make
+    make install
+	EOC
+  not_if { File.exists? '/usr/local/bin/webdis' }
+end
+
+bash 'set basic passsword' do
+  code <<-EOC
+    htpasswd -b /etc/nginx/htpass dariususer gjh65kHj7UYOPojndrettYt0o
+  EOC
+  not_if { File.exists? '/etc/nginx/htpass' }
+end
+
+service 'iptables' do
+	action [:enable, :start]
+end
+
+bash 'close all except ssh http and localhost connections' do
+  code <<-EOC
+    iptables -A INPUT -p tcp --dport 80 -j ACCEPT
+    iptables -A INPUT -p tcp --dport 22 -j ACCEPT
+    iptables -A INPUT -p tcp -s localhost -j ACCEPT
+    iptables -A INPUT -i lo -j ACCEPT
+    iptables -A INPUT -j DROP
+    iptables-save > /etc/sysconfig/iptables
+  EOC
+  not_if { File.exists? '/etc/sysconfig/iptables' }
+end
+
+service 'redis' do
+	action [:enable, :start]
+end
+
+execute 'sudo -u nobody /usr/local/bin/webdis &'
+
+template "/etc/nginx/conf.d/virtual.conf" do
+  source "virtual.conf.erb"
+  mode 0755
+end
+
+service 'nginx' do
+	action [:enable, :start]
+end
